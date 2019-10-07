d7321763880dd2c70343fc8571928bedad5033f1
Ruslan <ragusiev@gmail.com>
2019-10-08 00:47:40 +0200

    First commit

---
+++ publications.sql
@@ -0,0 +1,23 @@
+use publications;
+
+/* 1. SELECT authors.au_id, au_lname, au_fname, title, pub_name
+FROM authors
+INNER JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers
+ON titles.pub_id = publishers.pub_id;*/
+
+/* 2. SELECT authors.au_id, au_lname, au_fname, pub_name, COUNT(title)
+FROM authors, titles, publishers, titleauthor
+WHERE authors.au_id = titleauthor.au_id AND titleauthor.title_id = titles.title_id AND titles.pub_id = publishers.pub_id
+GROUP BY authors.au_id, au_lname, au_fname, pub_name;*/
+
+/*SELECT authors.au_id, au_lname, au_fname, SUM(qty) as Total
+FROM authors, titleauthor, titles, sales
+WHERE titles.title_id  = sales.title_id AND titles.title_id = titleauthor.title_id AND titleauthor.au_id = authors.au_id
+GROUP BY authors.au_id, au_lname, au_fname
+ORDER BY Total DESC
+LIMIT 3;*/
+
